28e52603c5bddeefa65f25db9bd116fe9234a185
rwacybercommunity-coder <rwacybercommunity@gmail.com>
2025-10-29 10:23:20 +0000

    feat: enhance Announcements component with new UI elements and user details

--- frontend/src/components/competition-tabs/Announcements.tsx
+++ frontend/src/components/competition-tabs/Announcements.tsx
@@ -1,10 +1,10 @@
 import { cn } from '@/lib/utils'
-import { FileQuestion } from 'lucide-react';
+import { Bell, FileQuestion, Podcast } from 'lucide-react';
 import React from 'react'
 interface AnnouncementsProps {
   activeTab: string;
 }
-function Announcements({activeTab}: AnnouncementsProps) {
+function Announcements({ activeTab }: AnnouncementsProps) {
   return (
     <div className={cn("", activeTab == 'Announcements' ? 'block' : 'hidden')}>
       <div className="mt-3">
@@ -13,36 +13,39 @@ function Announcements({activeTab}: AnnouncementsProps) {
             <div>Create Announcement</div>
           </div>
           <div className="bg-card border border-border border-dashed rounded-md">
-            <div className="grid grid-cols-4 items-center px-5 py-2 text-muted-foreground text-sm">
-              <div className='col-span-3'>
-                <div className="flex items-center gap-2">
-                  <div>
-                    <div className="w-8 h-8 text-white rounded-full bg-indigo-400 flex justify-center items-center"> <FileQuestion size={14} /> </div>
-                  </div>
-                  <div>
-                    <div className="font-semibold text-foreground">Cyber URL injection attack!</div>
-                    <div className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum porro et corrupti pariatur fuga repellendus, iste magnam iure? Deleniti distinctio cumque consequatur, ipsum exercitationem voluptas sint aut tempora earum molestiae.</div>
-                  </div>
-                </div>
-              </div>
-              <div>Actions</div>
+            <textarea placeholder='whats new today?' className='w-full p-3 outline-none min-h-[100px]'></textarea>
+          </div>
+          <div>
+            <button className="flex ml-auto mt-1 items-center text-sm gap-1 bg-indigo-500 text-white rounded-md p-1 px-3 h-fit"><Bell size={15} /> Post Announcement</button>
+          </div>
+        </div>
+        <div className="rounded-md p-1 mt-2 min-h-10 bg-background dark:bg-zinc-950/50">
+          <div className="flex items-center gap-2 p-2">
+            <div>
+              <div className="w-8 h-8 text-white rounded-full bg-indigo-400 flex justify-center items-center"> <FileQuestion size={14} /> </div>
             </div>
-            <div className="grid grid-cols-4 items-center px-5 py-2 text-muted-foreground text-sm">
-              <div className='col-span-3'>
-                <div className="flex items-center gap-2">
-                  <div>
-                    <div className="w-8 h-8 text-white rounded-full bg-indigo-400 flex justify-center items-center"> <FileQuestion size={14} /> </div>
-                  </div>
-                  <div>
-                    <div className="font-semibold text-foreground">Cyber URL injection attack!</div>
-                    <div className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum porro et corrupti pariatur fuga repellendus, iste magnam iure? Deleniti distinctio cumque consequatur, ipsum exercitationem voluptas sint aut tempora earum molestiae.</div>
-                  </div>
-                </div>
-              </div>
-              <div>Actions</div>
+            <div>
+              <div className="font-semibold text-foreground">Amanda Clerk</div>
+              <div className="text-sm text-muted-foreground">@moderator</div>
             </div>
           </div>
-
+          <div className="bg-card border border-border border-dashed jtext-muted-foreground rounded-md p-2">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod facilis sed aliquam distinctio dolore, molestias unde voluptate error nemo! Aspernatur, sapiente. Veritatis magnam odit repudiandae magni, eligendi aliquam ratione nobis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet animi eum nisi mollitia perspiciatis aliquid commodi? Perferendis sed ducimus consectetur nesciunt recusandae reiciendis sunt, quisquam quibusdam quod itaque sit similique.</p>
+          </div>
+        </div>
+        <div className="rounded-md p-1 mt-2 min-h-10 bg-background dark:bg-zinc-950/50">
+          <div className="flex items-center gap-2 p-2">
+            <div>
+              <div className="w-8 h-8 text-white rounded-full bg-indigo-400 flex justify-center items-center"> <FileQuestion size={14} /> </div>
+            </div>
+            <div>
+              <div className="font-semibold text-foreground">Amanda Clerk</div>
+              <div className="text-sm text-muted-foreground">@moderator</div>
+            </div>
+          </div>
+          <div className="bg-card border border-border border-dashed jtext-muted-foreground rounded-md p-2">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod facilis sed aliquam distinctio dolore, molestias unde voluptate error nemo! Aspernatur, sapiente. Veritatis magnam odit repudiandae magni, eligendi aliquam ratione nobis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet animi eum nisi mollitia perspiciatis aliquid commodi? Perferendis sed ducimus consectetur nesciunt recusandae reiciendis sunt, quisquam quibusdam quod itaque sit similique.</p>
+          </div>
         </div>
       </div>
     </div>
